1369fcde331881b81d48991c1c60ce328632ef25
Dvel <git@dvel.me>
2022-10-31 21:44:27 +0800

    feat: complete config for me

--- others/script/rime/rime.go
+++ others/script/rime/rime.go
@@ -2,59 +2,63 @@ package rime
 
 import (
 	"bufio"
-	"crypto/sha1"
-	"encoding/hex"
 	"fmt"
 	mapset "github.com/deckarep/golang-set/v2"
-	"io"
 	"log"
 	"os"
+	"os/user"
+	"path"
+	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 )
 
-// 一个词条的组成部分
+// 一个词的组成部分
 type lemma struct {
 	text   string // 汉字
 	code   string // 编码
 	weight int    // 权重
 }
 
-const (
-	mark        = "# +_+" // 词库中的标记符号，表示从开始检查或排序
-	HanziPath   = "/Users/dvel/Library/Rime/cn_dicts/8105.dict.yaml"
-	MainPath    = "/Users/dvel/Library/Rime/cn_dicts/base.dict.yaml"
-	SogouPath   = "/Users/dvel/Library/Rime/cn_dicts/sogou.dict.yaml"
-	ExtPath     = "/Users/dvel/Library/Rime/cn_dicts/ext.dict.yaml"
-	TencentPath = "/Users/dvel/Library/Rime/cn_dicts/tencent.dict.yaml"
-	EmojiPath   = "/Users/dvel/Library/Rime/opencc/emoji-map.txt"
-
-	DefaultWeight = 100 // sogou、ext、tencet 词库中默认的权重数值
-)
-
 var (
-	MainSet          mapset.Set[string]
-	SogouSet         mapset.Set[string]
-	ExtSet           mapset.Set[string]
-	TencentSet       mapset.Set[string]
-	SogouSetWithCode mapset.Set[string]
+	mark          = "# +_+"      // 词库中的标记符号，表示从这行开始进行检查或排序
+	RimeDir       = getRimeDir() // Rime 配置目录
+
+	EmojiMapPath = filepath.Join(RimeDir, "others/emoji-map.txt")
+	EmojiPath    = filepath.Join(RimeDir, "opencc/emoji.txt")
+
+	HanziPath   = filepath.Join(RimeDir, "cn_dicts/8105.dict.yaml")
+	BasePath    = filepath.Join(RimeDir, "cn_dicts/base.dict.yaml")
+	ExtPath     = filepath.Join(RimeDir, "cn_dicts/ext.dict.yaml")
+	TencentPath = filepath.Join(RimeDir, "cn_dicts/tencent.dict.yaml")
+
+	HanziSet   = readToSet(HanziPath)
+	BaseSet    = readToSet(BasePath)
+	ExtSet     = readToSet(ExtPath)
+	TencentSet = readToSet(TencentPath)
+
+	需要注音TXT   = filepath.Join(RimeDir, "others/script/rime/需要注音.txt")
+	错别字TXT    = filepath.Join(RimeDir, "others/script/rime/错别字.txt")
+	汉字拼音映射TXT = filepath.Join(RimeDir, "others/script/rime/汉字拼音映射.txt")
 )
 
-func init() {
-	fmt.Println("rime.go init...")
-	MainSet = readAndSet(MainPath)
-	SogouSet = readAndSet(SogouPath)
-	ExtSet = readAndSet(ExtPath)
-	TencentSet = readAndSet(TencentPath)
+// 获取 macOS Rime 配置目录
+func getRimeDir() string {
+	u, err := user.Current()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return filepath.Join(u.HomeDir, "Library/Rime")
 }
 
-// readAndSet 读取词库文件为 set
-func readAndSet(dictPath string) mapset.Set[string] {
+// 将所有词库读入 set，供检查或排序使用
+func readToSet(dictPath string) mapset.Set[string] {
 	set := mapset.NewSet[string]()
 
 	file, err := os.Open(dictPath)
 	if err != nil {
-		log.Fatal(set)
+		log.Fatalln(err)
 	}
 	defer file.Close()
 
@@ -63,7 +67,7 @@ func readAndSet(dictPath string) mapset.Set[string] {
 	for sc.Scan() {
 		line := sc.Text()
 		if !isMark {
-			if strings.Contains(line, mark) {
+			if strings.HasPrefix(line, mark) {
 				isMark = true
 			}
 			continue
@@ -75,17 +79,19 @@ func readAndSet(dictPath string) mapset.Set[string] {
 	return set
 }
 
-// printlnTimeCost 打印耗时时间
+// 打印耗时时间
 func printlnTimeCost(content string, start time.Time) {
-	fmt.Printf("%s：\t%.2fs\n", content, time.Since(start).Seconds())
+	// fmt.Printf("%s：\t%.2fs\n", content, time.Since(start).Seconds())
+	printfTimeCost(content, start)
+	fmt.Println()
 }
 
-// printfTimeCost 打印耗时时间
+// 打印耗时时间
 func printfTimeCost(content string, start time.Time) {
 	fmt.Printf("%s：\t%.2fs", content, time.Since(start).Seconds())
 }
 
-// contains slice 是否包含 item
+// slice 是否包含 item
 func contains(arr []string, item string) bool {
 	for _, x := range arr {
 		if item == x {
@@ -95,69 +101,43 @@ func contains(arr []string, item string) bool {
 	return false
 }
 
-// getSha1 获取文件 sha1
-func getSha1(dictPath string) string {
-	f, err := os.Open(dictPath)
+// AddWeight  为 ext、tencent 没权重的词条加上权重，有权重的改为 weight
+func AddWeight(dictPath string, weight int) {
+	// 控制台输出
+	printlnTimeCost("加权重\t"+path.Base(dictPath), time.Now())
+
+	// 读取到 lines 数组
+	file, err := os.ReadFile(dictPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
+	lines := strings.Split(string(file), "\n")
 
-	sha1Handle := sha1.New()
-	if _, err := io.Copy(sha1Handle, f); err != nil {
-		log.Fatal(err)
-	}
-
-	return hex.EncodeToString(sha1Handle.Sum(nil))
-}
-
-// updateVersion 排序后，如果文件有改动，则修改 version 日期
-func updateVersion(dictPath string, oldSha1 string) {
-	// 判断文件是否有改变
-	newSha1 := getSha1(dictPath)
-	if newSha1 == oldSha1 {
-		fmt.Println()
-		return
-	}
-	fmt.Println(" ...sorted")
-
-	// 打开文件
-	file, err := os.OpenFile(dictPath, os.O_RDWR, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	// 修改那一行
-	arr := make([]string, 0)
-	sc := bufio.NewScanner(file)
-	for sc.Scan() {
-		line := sc.Text()
-		if strings.HasPrefix(line, "version:") {
-			s := fmt.Sprintf("version: \"%s\"", time.Now().Format("2006-01-02"))
-			arr = append(arr, s)
-		} else {
-			arr = append(arr, line)
+	isMark := false
+	for i, line := range lines {
+		if !isMark {
+			if strings.HasPrefix(line, mark) {
+				isMark = true
+			}
+			continue
 		}
-	}
-
-	// 重新写入
-	err = file.Truncate(0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = file.Seek(0, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, line := range arr {
-		_, err := file.WriteString(line + "\n")
+		// 过滤空行
+		if line == "" {
+			continue
+		}
+		// 修改权重为传入的 weight，没有就加上
+		parts := strings.Split(line, "\t")
+		_, err := strconv.Atoi(parts[len(parts)-1])
 		if err != nil {
-			log.Fatal(err)
+			lines[i] = line + "\t" + strconv.Itoa(weight)
+		} else {
+			lines[i] = strings.Join(parts[:len(parts)-1], "\t") + "\t" + strconv.Itoa(weight)
 		}
 	}
 
-	err = file.Sync()
+	// 写入
+	resultString := strings.Join(lines, "\n")
+	err = os.WriteFile(dictPath, []byte(resultString), 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
